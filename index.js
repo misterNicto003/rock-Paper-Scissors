cf34cbc8898c9c1625ad004cbf1673a686bcbf9a
kanuni <mister.nicto006@gmail.com>
2024-04-17 11:46:32 +0300

    fix

--- index.js
+++ index.js
@@ -1,25 +1,84 @@
-let choice = ["камень","бумага","ножницы"]
+window.addEventListener("load", function () {
+  let countUser = document.querySelector(".count-user"),
+    countComp = document.querySelector(".count-comp"),
+    userField = document.querySelector(".user-field"),
+    compField = document.querySelector(".comp-field"),
+    sound = document.querySelector(".sound"),
+    res = document.querySelector(".result"),
+    play = document.querySelector(".play"),
+    fields = document.querySelectorAll(".field"),
+    userStep,
+    compStep,
+    countU = 0,
+    countC = 0,
+    blocked = false;
 
-function getComputerChoice(choice){
-    return choice[Math.floor(Math.random() * choice.length)]
-}
-
-
-function playRound(playerSelection, computerSelection) {
-    if(   playerSelection === computerSelection ){
-        console.log( "Галстук")
-    }else if (playerSelection === choice[0] && computerSelection === choice[2]){
-        console.log("игрок победил");
-    }else if (playerSelection === choice[1] && computerSelection === choice[0]) {
-        console.log("игрок победил");
-    }else if (playerSelection === choice[2] && computerSelection === choice[1]) {
-        console.log("игрок победил");
-    } else {
-        console.log("ты проиграл")
+  function choiceUser(e) {
+    if (blocked) return;
+    let target = e.target;
+    if (target.classList.contains("field")) {
+      userStep = target.dataset.field;
+      fields.forEach((item) => item.classList.remove("active", "error"));
+      target.classList.add("active");
+      choiceComp();
     }
   }
-  
-  const playerSelection = prompt("Пожалуйста, введите значение").toLowerCase();
-  const computerSelection = getComputerChoice(choice)
-  
-  console.log(playRound(playerSelection, computerSelection))
+  function choiceComp() {
+    blocked = true;
+    let rand = Math.floor(Math.random() * 3);
+    compField.classList.add("blink");
+    let compFields = compField.querySelectorAll(".field");
+
+    setTimeout(() => {
+      compField.classList.remove("blink");
+      compStep = compFields[rand].dataset.field;
+      compFields[rand].classList.add("active");
+      winner();
+    }, 2000);
+  }
+  function winner() {
+    blocked = false;
+    let comb = userStep + compStep;
+    console.log(comb);
+
+    switch (comb) {
+      case "rr":
+      case "ss":
+      case "pp":
+        res.innerText = "Ничья!";
+        break;
+
+      case "rs":
+      case "sp":
+      case "pr":
+        res.innerText = "Победили вы!";
+        countU++;
+        countUser.innerText = countU;
+        compField
+          .querySelector("[data-field=" + compStep + "]")
+          .classList.add("error");
+        break;
+
+      case "sr":
+      case "ps":
+      case "rp":
+        res.innerText = "вы проиграли!";
+        countC++;
+        countComp.innerText = countC;
+        userField
+          .querySelector("[data-field=" + userStep + "]")
+          .classList.add("error");
+        break;
+    }
+  }
+  function playGame() {
+    countU = countC = 0;
+    res.innerText = "сделай свой выбор";
+    countUser.innerText = 0;
+    countComp.innerText = 0;
+    fields.forEach((item) => item.classList.remove("active", "error"));
+  }
+
+  play.addEventListener("click", playGame);
+  userField.addEventListener("click", choiceUser);
+});
